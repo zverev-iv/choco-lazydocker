0f060532084f2abae5dc5a2898a0b2e1375285fc
Ilya Zverev <izverev@logrocon.com>
2020-02-10 19:27:44 +0300

    v0.8.0

--- lazydocker/tools/chocolateyinstall.ps1
+++ lazydocker/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://github.com/jesseduffield/lazydocker/releases/download/v0.7.6/lazydocker_0.7.6_Windows_x86.zip'
-  url64bit      = 'https://github.com/jesseduffield/lazydocker/releases/download/v0.7.6/lazydocker_0.7.6_Windows_x86_64.zip'
+  url           = 'https://github.com/jesseduffield/lazydocker/releases/download/v0.8/lazydocker_0.8_Windows_x86.zip'
+  url64bit      = 'https://github.com/jesseduffield/lazydocker/releases/download/v0.8/lazydocker_0.8_Windows_x86_64.zipы'
   softwareName  = 'lazydocker*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = '45B475D4B1C9EF7673022283AE0853ACAA1EF48EA44AFDA68656A5F83D9B2F7A'
+  checksum      = 'c01aa910f06321a3fe5e2d0145d56683337bf7c68db86488d071e595628d4c7b'
   checksumType  = 'sha256'
-  checksum64    = '9C59C501D307D9BD46CD5C03D6278A79873F08591768ABC0C16478C828878C54'
+  checksum64    = 'adcb89d19831d43fcc2ac2fc12d19e4db54025ce797bcdc72174e6b431665bee'
   checksumType64= 'sha256'
 }
 
