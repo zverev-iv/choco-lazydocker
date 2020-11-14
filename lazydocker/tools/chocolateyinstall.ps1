df58f287f604a7a1fa20becc9d10b9698871ccc5
zverev-iv <51955637+zverev-iv@users.noreply.github.com>
2020-11-14 21:20:12 +0300

    Update chocolateyinstall.ps1

--- lazydocker/tools/chocolateyinstall.ps1
+++ lazydocker/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://github.com/jesseduffield/lazydocker/releases/download/v0.9/lazydocker_0.9_Windows_x86.zip'
-  url64bit      = 'https://github.com/jesseduffield/lazydocker/releases/download/v0.9/lazydocker_0.9_Windows_x86_64.zip'
+  url           = 'https://github.com/jesseduffield/lazydocker/releases/download/v0.10/lazydocker_0.10_Windows_x86.zip'
+  url64bit      = 'https://github.com/jesseduffield/lazydocker/releases/download/v0.10/lazydocker_0.10_Windows_x86_64.zip'
   softwareName  = 'lazydocker*'
-  checksum      = '7a1071b8e90234401bc39b75cb1a869664d65cd0b25f3a4b1e62266a712cbf4d'
+  checksum      = '89af08532d8a486ff36ba712ab1464b06454cf67052ae5ff9c6cc066dd382fd2'
   checksumType  = 'sha256'
-  checksum64    = 'b4a4b69d4b1d4e8edd7ab2e995f9a574951c82606670a68c7044e8cb3244e603'
+  checksum64    = '743172a04ab046c6a772e6c5d309da1cc4a89926c0846a71bdd10d05dbe5c79c'
   checksumType64= 'sha256'
 }
 
