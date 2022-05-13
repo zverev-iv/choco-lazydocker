c0919389c56b6ca41390b3bf9e658e249abe965d
zverev-iv <51955637+zverev-iv@users.noreply.github.com>
2022-05-13 11:11:55 +0300

    Update chocolateyinstall.ps1

--- src/chocolateyinstall.ps1
+++ src/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
 	softwareName   = 'lazydocker'
 	packageName    = $env:ChocolateyPackageName
 	unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-	url            = 'https://github.com/jesseduffield/lazydocker/releases/download/v0.12/lazydocker_0.12_Windows_x86.zip'
-	checksum       = '747762627AF23B7C535CDFD353073244B977788CFDB22F83F50840E356F08673'
+	url            = 'https://github.com/jesseduffield/lazydocker/releases/download/v0.18.1/lazydocker_0.18.1_Windows_x86.zip'
+	checksum       = 'c2646a6c1835443441014b1a846f75391c449079644fcc23d0c09b3c3549a427'
 	checksumType   = 'sha256'
-	url64bit       = 'https://github.com/jesseduffield/lazydocker/releases/download/v0.12/lazydocker_0.12_Windows_x86_64.zip'
-	checksum64     = '4C09FDB74E96B2DFEBAB8775D0AE436B2713AD9F2CBF6C901B63AD4C1FFEF83B'
+	url64bit       = 'https://github.com/jesseduffield/lazydocker/releases/download/v0.18.1/lazydocker_0.18.1_Windows_x86_64.zip'
+	checksum64     = '6f99fe8e85f410ce119e6294603d8bec2dae72ec309d9f1fb55f8a54f3088bbc'
 	checksumType64 = 'sha256'
 }
 
